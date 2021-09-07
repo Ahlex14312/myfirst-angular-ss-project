5a5ad0ae081b96d88887c5189ed7be815240cb6b
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-09-07 20:30:17 +0800

    Activity Two Added

---
+++ src/app/fourth/fourth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-fourth',
+    template: '<p>fourth works!</p>',
+    styles: ['p {color: blue}']
+  })
+export class FourthComponent {
+
+}
